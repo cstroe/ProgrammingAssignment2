9c1461afff3d2a0212d1241fa40ab2ce0ac103a5
Cosmin Stroe <cstroe@backstopsolutions.com>
2015-05-22 23:42:27 -0500

    Describe the overall file and the makeCacheMatrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions:
+## * makeCacheMatrix: wraps a matrix datatype with a cache that stores its 
+##                    inverse
+## * cacheSolve: solve for the inverse of a matrix and save the solution
+##               to the cache
 
-## Write a short comment describing this function
-
+## Wrap a matrix with a function that caches its inverse.
+## Methods: set, get, setinverse, getinverse
+##
+## * set: set the matrix, clears cached solution
+## * get: get the matrix
+## * setinverse: add the solution to the cache
+## * getinverse: get the solution from the cache
+##
 makeCacheMatrix <- function(cache_matrix = matrix()) {
   matrix_inverse <- NULL
 
